059d1babb02615fad4ab92911f366fce8e4ad512
nytouu <capitaine7312@gmail.com>
2023-02-03 00:49:18 +0100

    cleanup

--- script.js
+++ script.js
@@ -99,67 +99,11 @@ function create()
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    scoreText=this.add.text(16,16,'score: 0',{
-        fontSize:'32px',
-        fill:'#000',
-    });
-    // show score text
-    stars = this.physics.add.group({
-        key: 'star', repeat: 11,
-        setXY: { x: 12, y: 0, stepX: 70 }
-    });
-    // every star bounces differently
-    stars.children.iterate(function (child)
-    {
-        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-    });
-    // colliders for stars
-    this.physics.add.collider(stars, platforms);
-    this.physics.add.overlap(player, stars, collectStar, null, this);
-
-    // colliders for bombs
-    bombs = this.physics.add.group();
-    this.physics.add.collider(bombs, platforms);
-    this.physics.add.collider(player, bombs, hitBomb, null, this);
-
     this.input.gamepad.once('connected', function (pad)
     {
         controller = pad;
     })
 
-}
-
-function collectStar(player, star)
-{
-    star.disableBody(true, true);
-    score += 10;
-    scoreText.setText('Score: ' + score);
-
-    if (stars.countActive(true) === 0)
-    {
-        stars.children.iterate(function (child)
-        {
-            child.enableBody(true, child.x, 0, true, true);
-        });
-
-        // spawn bomb on the side where the player is not there
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) :
-        Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-        bomb.allowGravity = false;
-    }
-}
-
-function hitBomb(player)
-{
-    this.physics.pause();
-    player.setTint(0xff0000);
-    player.anims.play('turn');
-    gameOver = true;
 }
 
 function update()
